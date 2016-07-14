61241ef302f6b3414d52317e07397711f101fa83
Bao Huy <huy23121994@gmail.com>
2016-07-14 10:59:30 +0700

    create and delete session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	render 'new'
+  	user = User.find_by(username: params[:session][:username].downcase)
+  	if user
+	  	if user.authenticate(params[:session][:password])
+	  		sign_in user
+	  		redirect_to user
+	  	else
+	  		flash[:danger] = 'Password is not correct!'
+	  		redirect_to :back
+		end
+	else
+  		flash[:danger] = "Don't exist this user"
+  		redirect_to :back
+  	end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
   end
 
 end
