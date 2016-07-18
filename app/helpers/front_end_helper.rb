f32ed33cf3073f9240ecbff381324dafa4cbd87e
Bao Huy <huy23121994@gmail.com>
2016-07-18 14:15:10 +0700

    refactoring extends_layout helper

--- app/helpers/front_end_helper.rb
+++ app/helpers/front_end_helper.rb
@@ -1,7 +1,2 @@
 module FrontEndHelper
-	def front_end_layout(layout)
-	    @view_flow.set(:layout, output_buffer)
-	    output = render(:file => "front_end/layouts/#{layout}")
-	    self.output_buffer = ActionView::OutputBuffer.new(output)
-  	end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,2 @@
 module UsersHelper
-
-	def user_login_layout
-	    @view_flow.set(:layout, output_buffer)
-	    output = render(:file => "back_end/layouts/login_layout")
-	    self.output_buffer = ActionView::OutputBuffer.new(output)
-  	end
-
 end
